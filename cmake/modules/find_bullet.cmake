e1a6ee81cec484e1448728e6ab82035b16aabcf4
Andreas-Christoph Bernstein <andreas.bernstein@uni-weimar.de>
2013-10-23 17:34:44 +0200

    add path

--- cmake/modules/find_bullet.cmake
+++ cmake/modules/find_bullet.cmake
@@ -68,7 +68,7 @@ IF (        BULLET_INCLUDE_DIRS
         FIND_PATH(_CUR_SEARCH
 				NAMES ${BULLET_DYNAMICS_LIB}
                 PATHS ${_SEARCH_DIR}
-				PATH_SUFFIXES debug release
+                PATH_SUFFIXES debug release ${BULLET_DYNAMICS_LIB_SUFFIX}
                 NO_DEFAULT_PATH)
         IF (_CUR_SEARCH)
             LIST(APPEND _BULLET_FOUND_LIB_DIR ${_SEARCH_DIR})
